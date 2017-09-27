20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
20170912
* Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
*The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.

20170922
* Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
* Added a procedure to delete 0 trading volumn M30 rows in system.

20190927
* There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.

src/LinuxBackEnd/C_Algorithms_BestMACDPattern.py
@@ -627,7 +627,7 @@ class C_MACD_Ending_Profit_Calculation(C_BestMACDPattern):
                         # tradeVolume = stockVolume_Current * tradePercent
                         stockVolume_Current = tradeVolume + stockVolume_Current
                         cash_Current = cash_Current - trade_cost
-                        totalValue_Current = stockVolume_Current * close_price + cash_Current
+                        totalValue_Current = np.round((stockVolume_Current * close_price + cash_Current), 3)
                         profit_Rate = math.log(totalValue_Current / totalValue_Begin)
 
                         # Append HalfHour Profit Results into Array
@@ -649,7 +649,7 @@ class C_MACD_Ending_Profit_Calculation(C_BestMACDPattern):
                         tradeVolume = stockVolume_Current
                         stockVolume_Current = 0
                         cash_Current = cash_Current + trade_cost
-                        totalValue_Current = stockVolume_Current * close_price + cash_Current
+                        totalValue_Current = np.round((stockVolume_Current * close_price + cash_Current), 3)
                         profit_Rate = math.log(totalValue_Current / totalValue_Begin)
 
                         # Append HalfHour Profit Results into Array
@@ -669,7 +669,7 @@ class C_MACD_Ending_Profit_Calculation(C_BestMACDPattern):
                 # Calculate lastest profit rate with lastest close_price: e_price for each pattern
                 stockVolume_Current = MACD_trades.iloc[-1].stockVolume_Current
                 cash_Current = MACD_trades.iloc[-1].cash_Current
-                totalValue_Current = stockVolume_Current * e_price + cash_Current
+                totalValue_Current = np.round((stockVolume_Current * e_price + cash_Current), 3)
                 profit_Rate = math.log(totalValue_Current / totalValue_Begin)
                 transaction = [e_stock_code, e_quote_time, 0, 0, 0, 0, 0, totalValue_Current, profit_Rate, 0, 0, 0,
                                pattern]
@@ -1448,9 +1448,9 @@ def main():
     # MACDPattern._get_best_pattern('sz002310')
     #MACDPattern.apply_best_MACD_pattern_to_data(period='m30', stock_code='sz002310', quo=0.7, ga=0.3, beta=0.2)
     #commu('1')
-    cal_specific_pattern()
+    # cal_specific_pattern()
     # MACDPattern._get_best_pattern('sz002310')
-    #caL_all_pattern()
+    caL_all_pattern()
 
 def caL_all_pattern():
     # gama is parameter to MACD(MAX-P) when saleing stock
@@ -1469,9 +1469,9 @@ def caL_all_pattern():
             for each_beta in beta:
                 MACD_Trading_Signal_Cal._MACD_trading_signals(period="m30", stock_code="sz002310", quo=each_quo,
                                                               ga=each_ga, beta=each_beta)
-                #MACD_Ending_Profit_Cal._MACD_ending_profits(period='m30', stock_code='sz002310')
+                MACD_Ending_Profit_Cal._MACD_ending_profits(period='m30', stock_code='sz002310')
                 #MACDPattern._save_MACD_best_pattern(period='m30')
-                #MACDPattern._get_best_pattern('sz002310')
+                MACDPattern._get_best_pattern('sz002310')
 
 
 def cal_specific_pattern():