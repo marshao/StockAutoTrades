13b44659c4cf4b9747ef9c6695928157cdc71a77
marshao <mars.guan@foxmail.com>
2017-10-17 12:40:28 +0800

    20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
    20170912
    * Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
    *The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.
    
    20170922
    * Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
    * Added a procedure to delete 0 trading volumn M30 rows in system.
    
    20170927
    * There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.
    
    20170929
    * Start up first_m30 analysis
    
    20171010
    * The multiprocess problem is from the Mysql DB. It seems from some block of table. The problem dispeared after restart DB.
    *Improved 25% computing efficiency by extracting all signals into memory but not extract signal pattern signals every time. Downside is to use much more memory.
    
    2017.10.11 in Dev
    * This is the first save from the DEV enviroment
    
    2017.10.12 in Dev
    *Changed Sturtures:
    -Set up a main datasource from in the CommuSocket
    -Seperate the main run out of the algorithm best macd
    
    2017.10.12 in Dev 2:
    * Set up Best MACD Pool system
      - Seperated funtions Pool
      - Modified the simulation of get-current-stock-inhand function.
    
    2017.10.13 in Dev:
    * Cleaned all unused files
    * Setting up global-variable file
    
    2017.10.16 in Dev:
    * Reset all application files with Global Configuration file.
    
    2017.10.16 in Dev 2:
    * Coding the testing part of records pool. Working in the sending_command_line function.
    
    2017.10.17 in Dev1:
    * Finish the first draft of Stock Pool validation function

--- src/LinuxBackEnd/C_BestMACDPattern_Pool.py
+++ src/LinuxBackEnd/C_BestMACDPattern_Pool.py
@@ -3,21 +3,15 @@
 
 __metclass__ = type
 
-import datetime, time, progressbar, math
 import pandas as pd
-import numpy as np
+import csv
 from sqlalchemy import create_engine, Table, Column, MetaData
-from sqlalchemy.sql import select, and_, or_, not_
-import multiprocessing as mp
-from CommuSocket import commu
-from GetRealData import get_data_qq
-import C_GlobalVariable as glb
-import cProfile
+
 from C_Algorithms_BestMACDPattern import C_Algorithems_BestPattern, C_MACD_Ending_Profit_Calculation, \
     C_MACD_Signal_Calculation
 
 
-class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
+class C_Algorithems_BestPattern_pool_validation(C_Algorithems_BestPattern):
     '''
     Change: 2017-06-22 in _send_trading_command: modified the received signal to 1 and -1 to match the signal calculation result.
             2017-06-22 in _MACD_Singal_calcualtion_cross use MACD_short = 999 to represent real life stock operation and save them in DB.
@@ -26,81 +20,9 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
     def __init__(self):
         C_Algorithems_BestPattern.__init__(self)
 
-    def _checking_stock_in_hand_pool(self, stock_code):
-        # Simulation of Getting stock in hand from the Live system.
-
-        receive = '1.1'
-        done = False
-        sql_select_stock_infor = (
-            "select stockCode, stockRemain, stockAvaliable, currentValue, Datetime from tb_StockInhand where stockCode = %s order by Datetime DESC limit 1")
-        if receive == '1.1':
-            df_stock_infor = pd.read_sql(sql_select_stock_infor, params=(stock_code,), con=self._engine)
-            self._log_mesg = self._log_mesg + "     Get df_stock_infor %s at %s \n" % (df_stock_infor, self._time_tag())
-            done = True
-        else:
-            df_stock_infor = pd.read_sql(sql_select_stock_infor, params=(stock_code,), con=self._engine)
-            self._log_mesg = self._log_mesg + "     Could not get current stock in hand information at %s \n" % self._time_tag()
-
-        if df_stock_infor.empty:
-            self._log_mesg = self._log_mesg + "     There is currently no stock %s in hand at %s \n." % (
-                stock_code, self._time_tag())
-            df_stock_infor.set_value(0, 'stockCode', stock_code)
-            df_stock_infor.set_value(0, 'stockAvaliable', 0)
-            df_stock_infor.set_value(0, 'currentValue', 0.0)
-            done = True
-        # self._write_log(self._log_mesg
-        # print df_stock_infor
-        return done, df_stock_infor
-
-    def _get_stock_current_price_pool(self, stock_code):
-        '''
-        done = get_data_qq(stock_code)
-        sql_select_current_price = (
-            "select stock_code,current_price, buy1_price, buy2_price, sale1_price, sale2_price  from tb_StockRealTimeRecords where stock_code = %s ORDER by time DESC limit 1")
-        if done:
-            df_current_price = pd.read_sql(sql_select_current_price, params=(stock_code[2:],), con=self._engine)
-            self._log_mesg = self._log_mesg + "     Get stock %s current price at %s .\n" % (
-                stock_code, self._time_tag())
-        else:
-            df_current_price = pd.read_sql(sql_select_current_price, params=(stock_code[2:],), con=self._engine)
-            done = False
-            self._log_mesg = self._log_mesg + "     Could not get stock %s current price data at %s\n" % (
-                stock_code, self._time_tag())
-            ### how to call the function to get current price
-        # self._write_log(self._log_mesg)
-        '''
-        # current price is the half hour closing price
-        done = True
-        # return done, df_current_price
-        return done
-
-    def _get_cash_avaliable_pool(self):
-        done = False  # The mark of success or not.
-        '''
-        cash_avabliable = -1
-        cmd_line = '5'
-        receives = commu(cmd_line).split()
-
-        if receives[0] == '5.1':
-            cash_avabliable = float(receives[1])
-            done = True
-            self._log_mesg = self._log_mesg + "     Get stock asset at %s \n" % (self._time_tag())
-        else:
-            self._log_mesg = self._log_mesg + "     Cannot get stock asset at %s \n" % (self._time_tag())
-            pass
-        '''
-        sql_select_cash_avaliable = ("select cash_avaliable from tb_StockAsset order by date_time DESC limit 1")
-        df_cash_avaliable = pd.read_sql(sql_select_cash_avaliable, con=self._engine)
-        cash_avabliable = df_cash_avaliable.iloc[0]
-        done = True
-        return done, cash_avabliable
-
-    def _check_stock_resale_pool(self, ):
-
-        pass
-
-    def _send_trading_command_pool(self, df_stock_infor, current_price, cash_avaliable, signal, pattern_number,
-                                         period):
+    def _send_trading_command_pool(self, stock_pool, asset_pool, stock_code, current_price, cash_avaliable, signal,
+                                   pattern_number,
+                                   period):
         '''
         Sending Trading singal to Windows Front End
         :param df_stock_infor:
@@ -112,10 +34,11 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
         :return:
         '''
         df_trade_history = pd.DataFrame(columns=self._trade_history_column)
-        stock_code = df_stock_infor.stockCode[0]
-        stock_Remain = df_stock_infor.stockRemain[0]
-        stock_avaliable = df_stock_infor.stockAvaliable[0]
-        current_value = df_stock_infor.currentValue[0]
+        # stock_code = df_stock_infor.stockCode[0]
+        # stock_Remain = df_stock_infor.stockRemain[0]
+        stock_Remain = stock_pool[stock_code][1]
+        # stock_avaliable = df_stock_infor.stockAvaliable[0]
+        stock_avaliable = stock_pool[stock_code][0]
         trade_volumn = 0
         volumn_up_limit = self._stock_inhand_uplimit
         # volumn_down_limit = 500
@@ -133,12 +56,23 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
 
             if (stock_Remain + oneshoot) < volumn_up_limit:
                 if trade_volumn >= oneshoot:
-                    trade_volumn = str(oneshoot)
+                    trade_volumn = oneshoot
                 else:
-                    trade_volumn = str(trade_volumn)
+                    trade_volumn = trade_volumn
+
+                # Calculate Cash Left
+                # updated_cash_avaliable = cash_avaliable - (trade_volumn*current_price)
+                print current_price
+                asset_pool['cash_avaliable'] -= (trade_volumn * current_price)
+                asset_pool['total_stock_value'] += (trade_volumn * current_price)
+                stock_pool[stock_code][0] += trade_volumn
+                stock_pool[stock_code][1] += trade_volumn
+                stock_pool[stock_code][2] += trade_volumn * current_price
+                '''
                 cmd = '2 ' + stock_code + ' ' + trade_volumn + ' ' + str(current_price)
                 self._log_mesg = self._log_mesg + "     Stock %s has the amount %s in hand at %s \n" % (
                     stock_code, stock_Remain, self._time_tag())
+                '''
             else:
                 self._log_mesg = self._log_mesg + "     Cannot buy more stock %s because " \
                                                   "the amount reach the up limit at %s \n" % (
@@ -150,6 +84,13 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
             if stock_avaliable != 0:
                 cmd = '3 ' + stock_code + ' ' + str(stock_avaliable) + ' ' + str(current_price)
                 trade_volumn = stock_avaliable
+                # updated_cash_avaliable = cash_avaliable + (trade_volumn * current_price)
+                asset_pool['cash_avaliable'] += (trade_volumn * current_price)
+                asset_pool['total_stock_value'] -= (trade_volumn * current_price)
+                stock_pool[stock_code][0] -= trade_volumn
+                stock_pool[stock_code][1] -= trade_volumn
+                stock_pool[stock_code][2] -= trade_volumn * current_price
+
             elif stock_Remain != 0 and stock_avaliable == 0:
                 trade_volumn = stock_Remain
                 trade_type = 9  # 9 is the resale sign
@@ -166,6 +107,7 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
         self._log_mesg = self._log_mesg + "     CMD %s is received at %s \n" % (cmd, self._time_tag())
         print self._log_mesg
 
+        '''
         print 'Run into confirmed code'
         line.append(stock_code)
         line.append(trade_type)
@@ -180,14 +122,13 @@ class C_Algorithems_BestPattern_pool(C_Algorithems_BestPattern):
         df_trade_history.to_sql('tb_StockTradeHistory', con=self._engine, index=False, if_exists='append')
         self._log_mesg = self._log_mesg + "     Trades %s is saved at %s \n" % (df_trade_history, self._time_tag())
         print df_trade_history
+        '''
         done = True
-
-
         # self._write_log(self._log_mesg)
         return done
 
 
-class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
+class C_BestMACDPattern_PoolValidation(C_Algorithems_BestPattern_pool_validation):
     '''
     Step1: decide what analysis period will be used (5min, 30min, daily)
     Step2: use MACD_Pattern_lists to calculate all trading signals of all patterns.
@@ -196,7 +137,7 @@ class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
     '''
 
     def __init__(self, period='m30'):
-        C_Algorithems_BestPattern_pool.__init__(self)
+        C_Algorithems_BestPattern_pool_validation.__init__(self)
         self._tb_MACDIndex = Table('tb_MACDIndex', self._metadata, autoload=True)
         self._tb_StockCodes = Table('tb_StockCodes', self._metadata, autoload=True)
         self._tb_TradeSignal = Table('tb_StockIndex_MACD_New', self._metadata, autoload=True)
@@ -320,7 +261,8 @@ class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
         finish_sign = True
         return finish_sign
 
-    def apply_best_MACD_pattern_to_data_test(self, period, stock_code, feed_records, quo=None, ga=None, beta=None):
+    def apply_best_MACD_pattern_to_data_test(self, period, stock_code, feed_records, stock_pool, asset_pool, quo=None,
+                                             ga=None, beta=None):
         done = False
         if quo is None:
             quo = 0.7
@@ -373,29 +315,39 @@ class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
             print "---------------------------------------------------"
             print "Step2: Get stock Avaliable"
             self._log_mesg = self._log_mesg + "Step2: Get stock Avaliable at %s \n" % self._time_tag()
-            done, df_stock_infor = self._checking_stock_in_hand_pool(stock_code[2:])
-            print df_stock_infor
+            # done, df_stock_infor = self._checking_stock_in_hand_pool(stock_code[2:], stock_pool)
+            done = True
+            stock_infor = stock_pool[stock_code]
+            # print stock_infor
+            '''
             if (done and signal == -1 and df_stock_infor.stockAvalible[0] != 0) or \
                     (signal == 1 and (
                                 (df_stock_infor.stockRemain[0] + self._trading_volume) < self._stock_inhand_uplimit)):
+            '''
+            if (done and signal == -1 and stock_infor[0] != 0) or \
+                    (signal == 1 and (
+                                (stock_infor[1] + self._trading_volume) < self._stock_inhand_uplimit)):
                 # Get real time price information
                 print "---------------------------------------------------"
                 print "Step3: Get real time price"
                 self._log_mesg = self._log_mesg + "Step3: Get real time price at %s \n" % self._time_tag()
                 # done, df_current_price = self._get_stock_current_price_pool(stock_code)
                 done = True
-                current_price = df_stock_records['close_price'].tail(1)
+                current_price = float(df_stock_records['close_price'].tail(1)[0])
                 if done:
                     # Get cash avaliable information
                     print "---------------------------------------------------"
                     print "Step4: Got cash Avaliable"
                     self._log_mesg = self._log_mesg + "Step4: Get cash Avaliable at %s \n" % self._time_tag()
-                    done, cash_avaliable = self._get_cash_avaliable_pool()
+                    # done, cash_avaliable = self._get_cash_avaliable_pool()
+                    done = True
+                    cash_avaliable = asset_pool['cash_avaliable']
                     if done:
                         print "-------------------------------------------------"
                         print "step5: Send Traiding Singal"
                         self._log_mesg = self._log_mesg + "Step5: Send Trading Signal at %s \n" % self._time_tag()
-                        done = self._send_trading_command_pool(df_stock_infor, current_price, cash_avaliable,
+                        done = self._send_trading_command_pool(stock_pool, asset_pool, stock_code, current_price,
+                                                               cash_avaliable,
                                                                signal, pattern_number, period)
                         if done:
                             self._log_mesg = self._log_mesg + '     Trading command had been executed successfully at %s \n' % self._time_tag()
@@ -408,7 +360,7 @@ class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
             else:
                 self._log_mesg = self._log_mesg + "     Unable to get stock_avalible information or stockRemain %s over " \
                                                   "the up limit %s at %s \n" % (
-                                                      df_stock_infor.stockRemain[0], self._stock_inhand_uplimit,
+                                                      stock_infor[1], self._stock_inhand_uplimit,
                                                       self._time_tag())
         else:
             #
@@ -460,25 +412,46 @@ class C_BestMACDPattern_Pool(C_Algorithems_BestPattern_pool):
 
 
 def main():
-    c = C_Algorithems_BestPattern_pool()
+    c = C_Algorithems_BestPattern_pool_validation()
     # c._get_cash_avaliable_pool()
-    get_records()
+    test_records()
 
 
-def get_records():
-    c = C_BestMACDPattern_Pool()
+def test_records():
+    c = C_BestMACDPattern_PoolValidation()
     stock_codes = ['sz002310', 'sh600867', 'sz300146', 'sh600271']
 
+    # Prepare the stock pool using
+    # Values Meaning: stock_avaliable, stock_remain, stock_value
+    stock_pool = {'sz002310': [0, 0, 0.0],
+                  'sh600867': [0, 0, 0.0],
+                  'sz300146': [0, 0, 0.0],
+                  'sh600271': [0, 0, 0.0],
+
+                  }
+    asset_pool = {'cash_begin': 100000.00,
+                  'cash_avaliable': 100000.00,
+                  'total_stock_value': 0.0,
+                  'total_asset': 100000.00
+
+                  }
+
+    # Prepare Pricing Data
     sql_fetch_min_records = (
         "select * from (select * from tb_StockXMinRecords where period = %s and stock_code = %s order by quote_time DESC) as tbl order by quote_time ASC")
 
     stock_records_dict = {
-        stock_codes[0]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[0])),
-        stock_codes[1]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[1])),
-        stock_codes[2]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[2])),
-        stock_codes[3]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[3]))
+        stock_codes[0]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[0]),
+                                    index_col='quote_time'),
+        stock_codes[1]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[1]),
+                                    index_col='quote_time'),
+        stock_codes[2]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[2]),
+                                    index_col='quote_time'),
+        stock_codes[3]: pd.read_sql(sql_fetch_min_records, con=c._engine, params=('m30', stock_codes[3]),
+                                    index_col='quote_time')
     }
 
+    # Prepare processing order data
     sql_select_quote_time = (
     "select quote_time from tb_StockXMinRecords where period = 'm30' and stock_code = 'sz002310' order by quote_time DESC")
     df_quote_time = pd.read_sql(sql_select_quote_time, con=c._engine)
@@ -487,21 +460,45 @@ def get_records():
     start_pos = end_pos + 400
     end_time = df_quote_time.iloc[end_pos][0]
     start_time = df_quote_time.iloc[start_pos][0]
-    print start_time, end_time
+    # print start_time, end_time
+
+    f = open('stock_pool_text.csv', 'wb')
+    writer = csv.writer(f)
+
+    # Process
     i = 0
-    while i < 2:
+    while i < 290:
         for stock_code in stock_codes:
+
             df = stock_records_dict[stock_code]
-            df = df[df['quote_time'] >= start_time]
-            feed_records = df[df['quote_time'] <= end_time]
-            c.apply_best_MACD_pattern_to_data_test('m30', stock_code, feed_records)
+            df = df[df.index >= start_time]
+            feed_records = df[df.index <= end_time]
+            last_row = feed_records.tail(1)
+            # Update pool
+            stock_pool[stock_code][2] = stock_pool[stock_code][0] * float(last_row['close_price'][0])
+            asset_pool['total_stock_value'] = sum(stock_pool.values()[2])
+            asset_pool['total_asset'] = asset_pool['cash_avaliable'] + asset_pool['total_stock_value']
+
+            writer.writerow([last_row.index[0], last_row['stock_code'][0], last_row['close_price'][0]])
+
+            writer.writerow([asset_pool.keys()[0], asset_pool.values()[0], asset_pool.keys()[1], asset_pool.values()[1],
+                             asset_pool.keys()[2], asset_pool.values()[2], asset_pool.keys()[3],
+                             asset_pool.values()[3]])
+            writer.writerow([stock_pool.keys()[0], stock_pool.values()[0], stock_pool.keys()[1], stock_pool.values()[1],
+                             stock_pool.keys()[2], stock_pool.values()[2], stock_pool.keys()[3],
+                             stock_pool.values()[3]])
+            # Send for process
+            c.apply_best_MACD_pattern_to_data_test('m30', stock_code, feed_records, stock_pool, asset_pool)
             # print feed_records['close_price'].tail(1)
+
         i += 1
         end_pos -= 1
         start_pos -= 1
         end_time = df_quote_time.iloc[end_pos][0]
         start_time = df_quote_time.iloc[start_pos][0]
-        #print start_time, end_time
+    print "Asset Pool is: ", asset_pool
+    print "Stock Pool is: ", stock_pool
+    #print asset_pool, stock_pool
 
 if __name__ == '__main__':
     main()
