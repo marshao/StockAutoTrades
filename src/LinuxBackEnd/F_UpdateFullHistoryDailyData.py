8ff2b7fc53cd6da4054b8a491c23d24e287126c2
marshao <mars.guan@foxmail.com>
2018-01-26 12:23:10 +0800

    20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
    20170912
    * Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
    *The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.
    
    20170922
    * Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
    * Added a procedure to delete 0 trading volumn M30 rows in system.
    
    20170927
    * There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.
    
    20170929
    * Start up first_m30 analysis
    
    20171010
    * The multiprocess problem is from the Mysql DB. It seems from some block of table. The problem dispeared after restart DB.
    *Improved 25% computing efficiency by extracting all signals into memory but not extract signal pattern signals every time. Downside is to use much more memory.
    
    2017.10.11 in Dev
    * This is the first save from the DEV enviroment
    
    2017.10.12 in Dev
    *Changed Sturtures:
    -Set up a main datasource from in the CommuSocket
    -Seperate the main run out of the algorithm best macd
    
    2017.10.12 in Dev 2:
    * Set up Best MACD Pool system
      - Seperated funtions Pool
      - Modified the simulation of get-current-stock-inhand function.
    
    2017.10.13 in Dev:
    * Cleaned all unused files
    * Setting up global-variable file
    
    2017.10.16 in Dev:
    * Reset all application files with Global Configuration file.
    
    2017.10.16 in Dev 2:
    * Coding the testing part of records pool. Working in the sending_command_line function.
    
    2017.10.17 in Dev1:
    * Finish the first draft of Stock Pool validation function
    
    2017.10.18 in Dev 1:
    * Added simplified parameter in signal single pattern signal calculation. When simplified = False, the system will store all signals.
    * Added Signal validation function
    *Added email fuction into Global Variables
    * Adding Transaction validation, the logic is still not very clear yet.
    
    2017.10.19 in Dev
    * Finished Transaction validation.
    *Seperated upate_stock_inhand and check_stock_inhand
    * Moved job scheduler and corresponding functions from qq_data_download to Back_end_main.
    *Added a 30min task of update_stock_inhand, followed by a transaction_validation.
    *Added signal_validation after each pattern_apply
    
    2017.10.23 in Dev:
    *Added Get_data_from_tuShare and Get_data_from_file
    
    2017.10.24 in Dev:
    * loaded history daily data in 2017 m30 data into DB
    
    2017.10.31 in Dev:
    * Doing pooling tests
    
    2017.11.02 in Dev:
    * Adding First 30m follows analysis
    
    2017.11.23 in Dev:
    * Adding function to get financial indicators
    
    2017.11.24 in Dev:
    * Added function of getting operation indicators
    
    2017.11.29 in Dev:
    * Almost finished the get fundamental data function
    
    2018.01.24 in Dev:
    * Updating Full History Daily Data
    * Preparing the main factors
    
    2018.01.26 in Dev:

--- src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
+++ src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
@@ -11,145 +11,171 @@ import time, urllib, re, os
 import pandas as pd
 import datetime
 from sqlalchemy import Table, MetaData
+from sqlalchemy import update
 from sqlalchemy.orm import sessionmaker
 import C_GlobalVariable as glb
 
 
-def get_stock_financial_report(self, stock_code_m=None):
-    if stock_code_m is None:
-        stock_code_m = 'sz300227'
-    stock_code = stock_code_m[2:]
-
-    # Defining Web URL
-    url_zcfzb = 'http://quotes.money.163.com/service/zcfzb_%s.html' % stock_code
-    url_lrb = 'http://quotes.money.163.com/service/lrb_%s.html' % stock_code
-    url_xjllb = 'http://quotes.money.163.com/service/xjllb_%s.html' % stock_code
-    url = [url_zcfzb, url_lrb, url_xjllb]
-    columns = ['报告日期'.decode('utf-8'), ]
-    report_data = pd.DataFrame()
-
-    # Getting web data
-    for each_url in url:
-        html = urllib.urlopen(each_url)
-        data = html.read()
-        # Remove --
-        no_value = re.compile('--')
-        data = no_value.sub('0', data)
-        # Remove \n
-        ul1 = re.compile('\n')
-        data = ul1.sub('', data)
-        # Remove Space
-        space = re.compile(' ')
-        data = space.sub('', data)
-        '''
-        # Remove 、
-        dunhao = re.compile('、')
-        data = dunhao.sub('', data)
-        # Remove :
-        maohao = re.compile('：')
-        data = maohao.sub('', data)
-        '''
-        # Process Web data into a DF
-        rows = []
-        endata = data.decode('GB2312').encode('utf-8').split('\r')
-        # print endata
-        # Prepare column name list and data row list
-        for line in endata:
-            line = line.split(',')
-            if (line[0] != '\t\t') & (line[0] != ''):
-                rows.append(line)
-                if line[0] != '报告日期':
-                    # create column name list
-                    percent = re.compile('\(%\)')
-                    col_name = percent.sub('', line[0])
-                    if each_url == url_xjllb:
-                        col_name = col_name.decode('utf-8')
-                        if col_name in columns:
-                            col_name = '现金表'.decode('utf-8') + col_name
-                    else:
-                        col_name = col_name.decode('utf-8')
-                    columns.append(col_name)
-
-        df = pd.DataFrame(rows).T
-        df.columns = df.iloc[0]
-        df = df.iloc[1:, :]
-        df['报告日期'] = df['报告日期'].astype('datetime64[ns]')
-        df.set_index('报告日期', inplace=True)
-        for column in df.columns:
-            if column != '报告日期':
-                df = df.dropna()
-                df[column] = pd.to_numeric(df[column], errors='raise')
-
-        if report_data.shape[1] == 0:
-            report_data = df
-        else:
-            report_data = pd.concat([report_data, df], axis=1, join_axes=[report_data.index])
-    report_data = report_data.dropna()
-    report_data['stock_code'] = stock_code_m
-    # Prepare DB Sessions
-    DBSession = sessionmaker(bind=self._engine)
-    session = DBSession()
-    meta = MetaData(self._engine)
-
-    # Delare Table
-    financail_reports = Table('tb_StockFinancialReports', meta, autoload=True)
-    # As the SQLAlchemy reading the table columns as unicode, so have to decode the columns names of DF
-
-    # Save Table
-    records = []
-    for idx, row in report_data.iterrows():
-        rd = {
-            'stock_code': stock_code_m,
-            columns[0]: idx, }
-        for i in range(0, 242):
-            rd.update({columns[i + 1]: row[i]})
-        records.append(dict(rd))
-
-    session.execute(financail_reports.insert(), records)
-    session.commit()
-    session.close()
-
-
-def update_daily_data():
+class C_Update_Full_History_Daily_Data(object):
     '''
-    This is the function to update information in the Table 'tb_StockFullHistoryDailyRecords'.
-    :param self:
-    :return:
+    2018-01-26: This function is designed to update and calculate factors values.
     '''
-    input_path = '/home/marshao/UWShare/source'
-    columns = ['stock_code', 'stock_name', 'L1_industry_code', 'L1_industry_name', 'L2_industry_code',
-               'L2_industry_name',
-               'L3_industry_code', 'L3_industry_name', 'L4_industry_code', 'L4_industry_name', 'stock_PE',
-               'stock_PE_TTM',
-               'stock_PB', 'stock_DP']
 
-    # Read new information from CSV file
-    for rt, dirs, files in os.walk(input_path):
-        for file in files:
-            # df = pd.read_csv(os.path.join(rt, file), sep=',', skiprows=1, names=columns)
-            print os.path.join(rt, file)
-            dt = '/home/marshao/UWShare/outrange'
-            pr = '/home/marshao/UWShare/processed'
-            df_new_infor = pd.read_csv(os.path.join(rt, file), sep=',')
-            df_new_infor.columns = [columns]
-            print df_new_infor
+    def __init__(self):
+        gv = glb.C_GlobalVariable()
+        self._emailobj = gv.get_emailobj()
+        self._master_config = gv.get_master_config()
+        self._input_path = self._master_config['ubuntu_file_input_dir']
+        self._engine = self._master_config['db_engine']
+
+    def get_stock_financial_report(self, stock_code_m=None):
+        if stock_code_m is None:
+            stock_code_m = 'sz300227'
+        stock_code = stock_code_m[2:]
+
+        # Defining Web URL
+        url_zcfzb = 'http://quotes.money.163.com/service/zcfzb_%s.html' % stock_code
+        url_lrb = 'http://quotes.money.163.com/service/lrb_%s.html' % stock_code
+        url_xjllb = 'http://quotes.money.163.com/service/xjllb_%s.html' % stock_code
+        url = [url_zcfzb, url_lrb, url_xjllb]
+        columns = ['报告日期'.decode('utf-8'), ]
+        report_data = pd.DataFrame()
+
+        # Getting web data
+        for each_url in url:
+            html = urllib.urlopen(each_url)
+            data = html.read()
+            # Remove --
+            no_value = re.compile('--')
+            data = no_value.sub('0', data)
+            # Remove \n
+            ul1 = re.compile('\n')
+            data = ul1.sub('', data)
+            # Remove Space
+            space = re.compile(' ')
+            data = space.sub('', data)
             '''
-            try:
-                df.to_sql(name='tb_StockFullHistoryDailyRecords', if_exists='append', con=self._engine, index=False)
-                os.rename(os.path.join(rt, file), os.path.join(pr, file))
-            except:
-                os.rename(os.path.join(rt, file), os.path.join(dt, file))
+            # Remove 、
+            dunhao = re.compile('、')
+            data = dunhao.sub('', data)
+            # Remove :
+            maohao = re.compile('：')
+            data = maohao.sub('', data)
             '''
+            # Process Web data into a DF
+            rows = []
+            endata = data.decode('GB2312').encode('utf-8').split('\r')
+            # print endata
+            # Prepare column name list and data row list
+            for line in endata:
+                line = line.split(',')
+                if (line[0] != '\t\t') & (line[0] != ''):
+                    rows.append(line)
+                    if line[0] != '报告日期':
+                        # create column name list
+                        percent = re.compile('\(%\)')
+                        col_name = percent.sub('', line[0])
+                        if each_url == url_xjllb:
+                            col_name = col_name.decode('utf-8')
+                            if col_name in columns:
+                                col_name = '现金表'.decode('utf-8') + col_name
+                        else:
+                            col_name = col_name.decode('utf-8')
+                        columns.append(col_name)
 
-    # Read old information from DB
-    stock_code = ''
-    sql_select_infor = 'select from tb_StockFullHistoryDailyRecords where stock_code = %s' % stock_code
-    for idx, row in df_new_infor.iterrows():
-        stock_code = row[stock_code]
+            df = pd.DataFrame(rows).T
+            df.columns = df.iloc[0]
+            df = df.iloc[1:, :]
+            df['报告日期'] = df['报告日期'].astype('datetime64[ns]')
+            df.set_index('报告日期', inplace=True)
+            for column in df.columns:
+                if column != '报告日期':
+                    df = df.dropna()
+                    df[column] = pd.to_numeric(df[column], errors='raise')
 
+            if report_data.shape[1] == 0:
+                report_data = df
+            else:
+                report_data = pd.concat([report_data, df], axis=1, join_axes=[report_data.index])
+        report_data = report_data.dropna()
+        report_data['stock_code'] = stock_code_m
+        # Prepare DB Sessions
+        DBSession = sessionmaker(bind=self._engine)
+        session = DBSession()
+        meta = MetaData(self._engine)
+
+        # Delare Table
+        financail_reports = Table('tb_StockFinancialReports', meta, autoload=True)
+        # As the SQLAlchemy reading the table columns as unicode, so have to decode the columns names of DF
+
+        # Save Table
+        records = []
+        for idx, row in report_data.iterrows():
+            rd = {
+                'stock_code': stock_code_m,
+                columns[0]: idx, }
+            for i in range(0, 242):
+                rd.update({columns[i + 1]: row[i]})
+            records.append(dict(rd))
+
+        session.execute(financail_reports.insert(), records)
+        session.commit()
+        session.close()
+
+    def update_industry_classes(self):
+        '''
+        This is the function to update industry classed information in the Table 'tb_StockFullHistoryDailyRecords'.
+        :param self:
+        :return:
+        '''
+        input_path = '/home/marshao/UWShare/source'
+        columns = ['stock_code', 'stock_name', 'L1_industry_code', 'L1_industry_name', 'L2_industry_code',
+                   'L2_industry_name',
+                   'L3_industry_code', 'L3_industry_name', 'L4_industry_code', 'L4_industry_name', 'stock_PE',
+                   'stock_PE_TTM',
+                   'stock_PB', 'stock_DP']
+
+        # Read new information from CSV file
+        for rt, dirs, files in os.walk(input_path):
+            for file in files:
+                # df = pd.read_csv(os.path.join(rt, file), sep=',', skiprows=1, names=columns)
+                print os.path.join(rt, file)
+                dt = '/home/marshao/UWShare/outrange'
+                pr = '/home/marshao/UWShare/processed'
+                df_new_infor = pd.read_csv(os.path.join(rt, file), dtype=str, sep=',')
+                df_new_infor.columns = [columns]
+
+
+                # Prepare DB Sessions
+        DBSession = sessionmaker(bind=self._engine)
+        session = DBSession()
+        meta = MetaData(self._engine)
+
+        # Delare Table
+        full_history_daily = Table('tb_StockFullHistoryDailyRecords', meta, autoload=True)
+
+        for idx, row in df_new_infor.iterrows():
+            stock_code_num = row['stock_code']
+            stock_name = row['stock_name']
+            L1_industry_code = row['L1_industry_code']
+            L1_industry_name = row['L1_industry_name']
+            L2_industry_code = row['L2_industry_code']
+            L2_industry_name = row['L2_industry_name']
+            L3_industry_code = row['L3_industry_code']
+            L3_industry_name = row['L3_industry_name']
+
+            stock_code = 'sh' + stock_code_num
+            print stock_code
+            # Read data from DB
+
+            # print stock
+            # session.execute(full_history_daily.update(), records)
+            # session.commit()
+        session.close()
 
 def main():
-    update_daily_data()
+    upd = C_Update_Full_History_Daily_Data()
+    upd.update_industry_classes()
 
 
 if __name__ == '__main__':
