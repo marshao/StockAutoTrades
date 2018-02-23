2df3e91b5c81467a7eaec7c3436b9e9e39149ba9
marshao <mars.guan@foxmail.com>
2018-02-23 11:24:35 +0800

    20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
    20170912
    * Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
    *The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.
    
    20170922
    * Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
    * Added a procedure to delete 0 trading volumn M30 rows in system.
    
    20170927
    * There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.
    
    20170929
    * Start up first_m30 analysis
    
    20171010
    * The multiprocess problem is from the Mysql DB. It seems from some block of table. The problem dispeared after restart DB.
    *Improved 25% computing efficiency by extracting all signals into memory but not extract signal pattern signals every time. Downside is to use much more memory.
    
    20171023 in Master:
    * Solved a problem in Signal calculation about DB engine.
    
    20171108 in Master:
    * The M30 data downloading is missing data. I guess the problem is from the cron task of M30 downloading and update_stock_inhand are scheduled at the same exact time. I sepereated them by adding seconds parameter.
    
    2018-01-30 in DEV:
    * The index improved he Mysql porformance dramatically.
    
    2018-02-23 in DEV:
    * Start to build factors marts, EP_TTM

--- src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
+++ src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
@@ -72,100 +72,6 @@ class C_Update_Full_History_Daily_Data(object):
                     time.sleep(1)
             i += 1
 
-    def get_stock_financial_report(self, stock_code_m=None):
-        if stock_code_m is None:
-            stock_code_m = 'sz300227'
-        stock_code = stock_code_m[2:]
-
-        # Defining Web URL
-        url_zcfzb = 'http://quotes.money.163.com/service/zcfzb_%s.html' % stock_code
-        url_lrb = 'http://quotes.money.163.com/service/lrb_%s.html' % stock_code
-        url_xjllb = 'http://quotes.money.163.com/service/xjllb_%s.html' % stock_code
-        url = [url_zcfzb, url_lrb, url_xjllb]
-        columns = ['报告日期'.decode('utf-8'), ]
-        report_data = pd.DataFrame()
-
-        # Getting web data
-        for each_url in url:
-            html = urllib.urlopen(each_url)
-            data = html.read()
-            # Remove --
-            no_value = re.compile('--')
-            data = no_value.sub('0', data)
-            # Remove \n
-            ul1 = re.compile('\n')
-            data = ul1.sub('', data)
-            # Remove Space
-            space = re.compile(' ')
-            data = space.sub('', data)
-            '''
-            # Remove 、
-            dunhao = re.compile('、')
-            data = dunhao.sub('', data)
-            # Remove :
-            maohao = re.compile('：')
-            data = maohao.sub('', data)
-            '''
-            # Process Web data into a DF
-            rows = []
-            endata = data.decode('GB2312').encode('utf-8').split('\r')
-            # print endata
-            # Prepare column name list and data row list
-            for line in endata:
-                line = line.split(',')
-                if (line[0] != '\t\t') & (line[0] != ''):
-                    rows.append(line)
-                    if line[0] != '报告日期':
-                        # create column name list
-                        percent = re.compile('\(%\)')
-                        col_name = percent.sub('', line[0])
-                        if each_url == url_xjllb:
-                            col_name = col_name.decode('utf-8')
-                            if col_name in columns:
-                                col_name = '现金表'.decode('utf-8') + col_name
-                        else:
-                            col_name = col_name.decode('utf-8')
-                        columns.append(col_name)
-
-            df = pd.DataFrame(rows).T
-            df.columns = df.iloc[0]
-            df = df.iloc[1:, :]
-            df['报告日期'] = df['报告日期'].astype('datetime64[ns]')
-            df.set_index('报告日期', inplace=True)
-            for column in df.columns:
-                if column != '报告日期':
-                    df = df.dropna()
-                    df[column] = pd.to_numeric(df[column], errors='raise')
-
-            if report_data.shape[1] == 0:
-                report_data = df
-            else:
-                report_data = pd.concat([report_data, df], axis=1, join_axes=[report_data.index])
-        report_data = report_data.dropna()
-        report_data['stock_code'] = stock_code_m
-        # Prepare DB Sessions
-        DBSession = sessionmaker(bind=self._engine)
-        session = DBSession()
-        meta = MetaData(self._engine)
-
-        # Delare Table
-        financail_reports = Table('tb_StockFinancialReports', meta, autoload=True)
-        # As the SQLAlchemy reading the table columns as unicode, so have to decode the columns names of DF
-
-        # Save Table
-        records = []
-        for idx, row in report_data.iterrows():
-            rd = {
-                'stock_code': stock_code_m,
-                columns[0]: idx, }
-            for i in range(0, 242):
-                rd.update({columns[i + 1]: row[i]})
-            records.append(dict(rd))
-
-        session.execute(financail_reports.insert(), records)
-        session.commit()
-        session.close()
-
     def load_industry_classes(self):
         '''
         This is the function to update industry classed information in the Table 'tb_StockFullHistoryDailyRecords'.
@@ -242,6 +148,49 @@ class C_Update_Full_History_Daily_Data(object):
             session.commit()
         session.close()
 
+    def load_stockcodes(self):
+        sql_select_codes = 'select stock_code from tb_StockCodeList'
+        df_stock_codes = pd.read_sql(con=self._engine, sql=sql_select_codes)
+        return df_stock_codes
+
+    def update_direct_factors(self, df_stock_codes):
+        DBSession = sessionmaker(bind=self._engine)
+        session = DBSession()
+        meta = MetaData(self._engine)
+
+        # Delare Table
+        SZFactors = Table('tb_StockSZFactors', meta, autoload=True)
+        SHFactors = Table('tb_StockSHFactors', meta, autoload=True)
+        SZHisDaily = Table('tb_StockSZHisDaily', meta, autoload=True)
+        SHHisDaily = Table('tb_StockSHHisDaily', meta, autoload=True)
+
+        for idx, row in df_stock_codes.iterrows():
+            stock_code = row['stock_code']
+            sz_ret = session.query(exists().where(SZHisDaily.columns['stock_code'] == stock_code)).scalar()
+            if sz_ret:
+                stat = self.update_ep_ttm(stock_code, SZHisDaily, SZFactors)
+                session.execute(stat)
+                print 'updated stock %s' % stock_code
+            elif session.query(exists().where(SHHisDaily.columns['stock_code'] == stock_code)).scalar():
+                stat = self.update_ep_ttm(stock_code, SHHisDaily, SHFactors)
+                session.execute(stat)
+                print 'updated stock %s' % stock_code
+            session.commit()
+        session.close()
+
+    def update_ep_ttm(self, stock_code, sour_table, des_table):
+        stat = des_table.update(). \
+            where(des_table.columns['stock_code'] == stock_code). \
+            values(L1_industry_code=L1_industry_code,
+                   stock_name=stock_name,
+                   L1_industry_name=L1_industry_name,
+                   L2_industry_code=L2_industry_code,
+                   L2_industry_name=L2_industry_name,
+                   L3_industry_code=L3_industry_code,
+                   L3_industry_name=L3_industry_name
+                   )
+        return stat
+
     def multi_processors_update(self):
         df_new_infor = self.load_industry_classes()
         infor_length = df_new_infor.count()[0]
@@ -270,8 +219,8 @@ class C_Update_Full_History_Daily_Data(object):
 
 def main():
     upd = C_Update_Full_History_Daily_Data()
-    upd.multi_processors_update()
-
+    # upd.multi_processors_update()
+    upd.load_stockcodes()
 
 if __name__ == '__main__':
     main()
