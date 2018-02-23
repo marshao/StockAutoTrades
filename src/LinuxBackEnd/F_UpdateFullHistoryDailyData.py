20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
20170912
* Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
*The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.

20170922
* Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
* Added a procedure to delete 0 trading volumn M30 rows in system.

20170927
* There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.

20170929
* Start up first_m30 analysis

20171010
* The multiprocess problem is from the Mysql DB. It seems from some block of table. The problem dispeared after restart DB.
*Improved 25% computing efficiency by extracting all signals into memory but not extract signal pattern signals every time. Downside is to use much more memory.

20171023 in Master:
* Solved a problem in Signal calculation about DB engine.

20171108 in Master:
* The M30 data 

src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
@@ -7,11 +7,10 @@ import sys
 
 sys.path.append('/home/marshao/DataMiningProjects/Project_StockAutoTrade_LinuxBackEnd/StockAutoTrades/src')
 
-import time, urllib, re, os
+import time, os
 import pandas as pd
-import datetime
 import multiprocessing as mp
-from sqlalchemy import Table, MetaData, exists, update
+from sqlalchemy import Table, MetaData, exists, update, and_, select
 from sqlalchemy.orm import sessionmaker
 import C_GlobalVariable as glb
 
@@ -168,30 +167,55 @@ class C_Update_Full_History_Daily_Data(object):
             stock_code = row['stock_code']
             sz_ret = session.query(exists().where(SZHisDaily.columns['stock_code'] == stock_code)).scalar()
             if sz_ret:
-                stat = self.update_ep_ttm(stock_code, SZHisDaily, SZFactors)
+                stat = self.update_ep_ttm(stock_code, 'sz', SZFactors)
                 session.execute(stat)
                 print 'updated stock %s' % stock_code
             elif session.query(exists().where(SHHisDaily.columns['stock_code'] == stock_code)).scalar():
-                stat = self.update_ep_ttm(stock_code, SHHisDaily, SHFactors)
+                stat = self.update_ep_ttm(stock_code, 'sh', SHFactors)
                 session.execute(stat)
                 print 'updated stock %s' % stock_code
             session.commit()
         session.close()
 
-    def update_ep_ttm(self, stock_code, sour_table, des_table):
+    def update_ep_ttm(self, stock_code, market, des_table):
+
+        meta = MetaData(self._engine)
+        sql_read_src_sz = ('select quote_time, PE_TTM from tb_StockSZHisDaily where stock_code = %s')
+        sql_read_src_sh = ('select quote_time, PE_TTM from tb_StockSHHisDaily where stock_code = %s')
+        sql_read_des_sz = ('select stock_code, quote_time from tb_StockSZFactors where stock_code = %s')
+        sql_read_des_sh = ('select stock_code, quote_time  from tb_StockSHFactors where stock_code = %s')
+
+        if market == 'sz':
+            df_src = pd.read_sql(con=self._engine, sql=sql_read_src_sz, params=[stock_code])
+            df_des = pd.read_sql(con=self._engine, sql=sql_read_des_sz, params=[stock_code])
+            d_tb = 'tb_StockSZFactors'
+        else:
+            df_src = pd.read_sql(con=self._engine, sql=sql_read_src_sh, params=[stock_code])
+            df_des = pd.read_sql(con=self._engine, sql=sql_read_des_sh, params=[stock_code])
+            d_tb = 'tb_StockSHFactors'
+
+        df_src['EP_TTM'] = df_src['PE_TTM'].rdiv(1)
+        df_src.drop(['PE_TTM'], axis=1)
+        df_result = pd.merge(df_des, df_src, how='inner', on=['quote_time'])
+        df_result.to_sql('tmp', con=self._engine, if_exists='replace')
+        # df_src['EP_TTM'] = np.where(df_src['PE_TTM']==0, 1/df_src['PE_TTM'], 0)
+        # df_src.loc[df_src['EP_TTM'] != 0, 'EP_TTM'] = 1/df_src['PE_TTM']
+
+        tmp = Table('tmp', meta, autoload=True)
+
         stat = des_table.update(). \
-            where(des_table.columns['stock_code'] == stock_code). \
-            values(L1_industry_code=L1_industry_code,
-                   stock_name=stock_name,
-                   L1_industry_name=L1_industry_name,
-                   L2_industry_code=L2_industry_code,
-                   L2_industry_name=L2_industry_name,
-                   L3_industry_code=L3_industry_code,
-                   L3_industry_name=L3_industry_name
-                   )
+            values(EP_TTM=select([tmp.c.EP_TTM]).where(tmp.c.quote_time == des_table.c.quote_time)). \
+            where(des_table.c.stock_code == stock_code)
+
+        stock_code = "'sh600000'"
+        stat = 'UPDATE %s d ' % d_tb + \
+               'SET d.EP_TTM = (SELECT t.EP_TTM from tmp t where t.quote_time = d.quote_time) ' + \
+               'WHERE d.stock_code = %s;' % stock_code
+
+
         return stat
 
-    def multi_processors_update(self):
+    def multi_processors_update_industries(self):
         df_new_infor = self.load_industry_classes()
         infor_length = df_new_infor.count()[0]
         print infor_length
@@ -216,11 +240,36 @@ class C_Update_Full_History_Daily_Data(object):
 
         self.processes_pool(tasks=processes, processors=num_processor)
 
+    def multi_processors_update_direct_factors(self):
+        df_stock_codes = self.load_stockcodes()
+        infor_length = df_stock_codes.count()[0]
+        print infor_length
+
+        # wait = input("Please")
+        # print wait
+
+        num_processor = self._processors
+        index_beg = 0
+        index_end = infor_length / num_processor
+
+        processes = []
+        for i in range(num_processor + 1):
+            if i != num_processor:
+                print "i:%s,  index_beg %s , index_end %s " % (i, index_beg, index_end)
+                p = mp.Process(target=self.update_direct_factors,
+                               args=(df_stock_codes[index_beg:index_end],))
+                processes.append(p)
+
+                index_beg = index_end
+                index_end = index_end + infor_length / num_processor
+
+        self.processes_pool(tasks=processes, processors=num_processor)
+
 
 def main():
     upd = C_Update_Full_History_Daily_Data()
-    # upd.multi_processors_update()
-    upd.load_stockcodes()
+    # upd.multi_processors_update_industries()
+    upd.multi_processors_update_direct_factors()
 
 if __name__ == '__main__':
     main()