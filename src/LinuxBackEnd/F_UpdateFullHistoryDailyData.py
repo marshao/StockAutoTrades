20170904 UBT1 * Wrong if else determination condition when signal == -1 and apply signal to records. * Adding function to reactive stock agent every 5 mins *Change the best pattern calculation from daily basis to weekly basis to enforce the insistency of pattern. * Find out a bug in apply_best _pattern, in step 2 (check_avaliable_stock), the buying command do not need to varify the done value. *Adding functions to enable trading multiple stocks 20170906: * Found a bug in Stock in hand information retrival. When receive 1.1 information, the done should be True. 20170907 * Add one more condition in stock inhand check. When signal = -1 and done = True, the stockAvaliable must not = 0 to process the next step.
20170912
* Found a bug in system, the log has different signal value with value in DB. Trying to set up a validation mechnisze to mornitor the consistense.
*The problem may from one cause, everytime when applying best pattern to stock, the table will not be cleaned before. So there are multiple records in the table. That may cause some problem. The fix is to force the table be cleaned every time before applying pattern on stock.

20170922
* Found a bug, whether the system or the data from QQ will add random deplicated M30 rows with 0 trading_volumn into DB.
* Added a procedure to delete 0 trading volumn M30 rows in system.

20170927
* There is a new problem, after updating Ubuntu core. There seems has new control on PID management. It makes my system hang and wait for new pid until timeout. This issue must be solved asap.

20170929
* Start up first_m30 analysis

20171010
* The multiprocess problem is from the Mysql DB. It seems from some block of table. The problem dispeared after restart DB.
*Improved 25% computing efficiency by extracting all signals into memory but not extract signal pattern signals every time. Downside is to use much more memory.

20171023 in Master:
* Solved a problem in Signal calculation about DB engine.

20171108 in Master:
* The M30 data 

src/LinuxBackEnd/F_UpdateFullHistoryDailyData.py
@@ -247,7 +247,7 @@ class C_Update_Full_History_Daily_Data(object):
                     'select stock_code, quote_time, total_asset from tb_StockMainFinancialIndicators where stock_code like "sz%%"')
                 sql_read_src_sh = (
                     'select stock_code, quote_time, total_asset from tb_StockMainFinancialIndicators where stock_code like "sh%%"')
-            elif factor == 'opg_ttm':
+            elif factor == 'opg_ttm' or factor == 'opg_sq':
                 sql_read_src_sz = ('select stock_code, quote_time, op_revenue from tb_StockFinancialReports')
                 sql_read_src_sh = ('select stock_code, quote_time, op_revenue from tb_StockFinancialReports')
             else:
@@ -292,7 +292,7 @@ class C_Update_Full_History_Daily_Data(object):
                     'select stock_code, quote_time, total_asset from tb_StockMainFinancialIndicators where stock_code = %s')
                 sql_read_src_sh = (
                     'select stock_code, quote_time, total_asset from tb_StockMainFinancialIndicators where stock_code = %s')
-            elif factor == 'opg_ttm':
+            elif factor == 'opg_ttm' or factor == 'opg_sq':
                 sql_read_src_sz = (
                     'select stock_code, quote_time, op_revenue from tb_StockFinancialReports where stock_code = %s')
                 sql_read_src_sh = (
@@ -331,6 +331,8 @@ class C_Update_Full_History_Daily_Data(object):
             stat, parameters, error = self.update_fc_mc(stock_code, Market_Factors, df_src, df_des, parameters)
         elif factor == 'opg_ttm':
             stat, parameters, error = self.update_opg_ttm(stock_code, Market_Factors, df_src, df_des, parameters)
+        elif factor == 'opg_sq':
+            stat, parameters, error = self.update_opg_sq(stock_code, Market_Factors, df_src, df_des, parameters)
         else:
             print " Unkonwn factor in run_update"
         return stat, parameters, error
@@ -861,6 +863,22 @@ class C_Update_Full_History_Daily_Data(object):
         error_list.append((stock_code))
         return stat, parameters, error_list
 
+    def update_opg_sq(self, stock_code, des_table, df_src_o, df_des, parameters):
+        error_list = []
+        stat = des_table.update(). \
+            values(OPG_SQ=bindparam('_OPG_SQ')). \
+            where(and_(des_table.c.id_tb == bindparam('_id_tb')))
+        df_src = df_src_o.loc[:, ('quote_time', 'op_revenue')]
+        df_src = self.cal_sq_growth(df_src, src_col='op_revenue', des_col='OPG_SQ')
+        df_des = self.fill_daily_for_sessonal_factors(df_src, df_des, 'OPG_SQ')
+        df_des.reset_index(inplace=True)
+        df_des = df_des.loc[:, ('id_tb', 'quote_time', 'OPG_SQ')]
+        for idx, row in df_des.iterrows():
+            parameters.append({'_OPG_SQ': row.OPG_SQ, '_id_tb': row.id_tb})
+
+        error_list.append((stock_code))
+        return stat, parameters, error_list
+
     def cal_ttm_growth(self, df_src, src_col, des_col):
         '''
         From src_col to calculated ttm growth to des col
@@ -880,6 +898,26 @@ class C_Update_Full_History_Daily_Data(object):
         df_src.fillna(-99.0, inplace=True)
 
         return  df_src
+
+    def cal_sq_growth(self, df_src, src_col, des_col):
+        '''
+        From src_col to calculated same quarter growth to des col
+        :param df_src:
+        :param src_col:
+        :param des_col:
+        :return:
+        '''
+        df_src['year'] = df_src['quote_time'].year
+        df_src['month'] = df_src['quote_time'].month
+        df_src[des_col] = df_src[
+            np.log(df_src[src_col] - df_src.loc[(df_src['year'] - 1) & (df_src['month']), src_col])]
+        print df_src
+        time.sleep(100)
+        # replace infinite value
+        df_src.loc[(df_src[des_col] == np.inf) | (df_src[des_col] == -np.inf), des_col] = -99.0
+        df_src.fillna(-99.0, inplace=True)
+
+        return df_src
 
     def fill_daily_for_sessonal_factors(self, df_src, df_des, factor):
         '''